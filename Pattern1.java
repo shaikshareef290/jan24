23021d6fb9e324d499a5a0d6fc4ffdc1f14e1038
unknown <22bk1a05f8@stpetershyd.com>
2025-01-24 12:36:53 +0530

    first commit

---
+++ Pattern1.java
@@ -0,0 +1,21 @@
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		/*
+		 * 5
+		 * 5 4
+		 * 5 4 3
+		 * 5 4 3 2
+		 * 5 4 3 2 1
+		 */
+		for(int r=5;r>=1;r--) {//4
+			for(int c=5;c>=r;c--) {//5>=4
+				System.out.print(c+"");//5 % 4
+			}
+			System.out.println();
+		}
+
+	}
+
+}
